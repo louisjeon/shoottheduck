48f0b5444bcbc3de357889e58ef6005008e9455d
louisjeon <wjss5115@gmail.com>
2024-10-21 22:08:55 +0900

    a

---
+++ src/main/java/kr/jbnu/se/std/BossDuck.java
@@ -0,0 +1,19 @@
+package kr.jbnu.se.std;
+
+import java.awt.image.BufferedImage;
+
+public class BossDuck extends MovingBossObject {
+    public static final int INITIAL_HEALTH = 100;
+    public static int nextObjectLines = 0;
+    public static int[][] objectLines = {
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.30), -17, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.35), -16, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.40), -15, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.45), -14, 500}
+    }; // 초기 체력 설정
+
+    public BossDuck(BufferedImage objectImg) {
+        super(objectLines,nextObjectLines,objectImg);
+        this.health = INITIAL_HEALTH;
+    }
+}
