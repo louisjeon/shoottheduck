d5872e5c44768d577c387f54a8db529beb317db7
louisjeon <wjss5115@gmail.com>
2024-10-21 22:14:42 +0900

    a

--- src/main/java/kr/jbnu/se/std/Pumpkin.java
+++ src/main/java/kr/jbnu/se/std/Pumpkin.java
@@ -2,32 +2,18 @@ package kr.jbnu.se.std;
 
 import java.awt.image.BufferedImage;
 
-public class Pumpkin extends MovingObject {
+public class Pumpkin extends MovingBossObject {
+    public static final int INITIAL_HEALTH = 100;
     public static int nextObjectLines = 0;
-    public static boolean isBoss = true;
-    private int pumpkinHealth;
-
-    public static long pumpkinLastObjectTime;
-    /**
-     * How much time must pass in order to create a new eagle?
-     */
-    public static long pumpkinTimeBetweenObjects = Framework.secInNanosec * 3;
-
     public static int[][] objectLines = {
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.025), -5, 500},
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.1), +5, 500}
-    };
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.30), -17, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.35), -16, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.40), -15, 500},
+            {Framework.frameWidth, (int)(Framework.frameHeight * 0.45), -14, 500}
+    }; // 초기 체력 설정
 
-
-
-    public Pumpkin(BufferedImage pumpkinImg) {
-        super(objectLines, nextObjectLines, pumpkinImg);
-        pumpkinHealth = 1500;
-    }
-
-    // 이거 객체지향적으로 수정할것!
-    public int getPumpkinHealth() {
-        System.out.println(pumpkinHealth);
-        return --pumpkinHealth;
+    public Pumpkin(BufferedImage objectImg) {
+        super(objectLines,nextObjectLines,objectImg);
+        this.health = INITIAL_HEALTH;
     }
 }
