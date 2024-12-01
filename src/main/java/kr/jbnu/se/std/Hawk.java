95ffa4f540f2807c377a8d3bf09dc2581c3771b0
Kimyunsu <yunsu3375@naver.com>
2024-12-01 14:25:11 +0900

    HawkDV

--- src/main/java/kr/jbnu/se/std/Hawk.java
+++ src/main/java/kr/jbnu/se/std/Hawk.java
@@ -1,39 +1,44 @@
 package kr.jbnu.se.std;
 
-import java.awt.image.BufferedImage;
-
-/**
- * The duck class.
- * 
- * @author www.gametutorial.net
- */
+import java.io.IOException;
 
 public class Hawk extends MovingObject {
-    public static long lastObjectTime;
-    /**
-     * How much time must pass in order to create a new eagle?
-     */
-    public static long timeBetweenObjects = Framework.secInNanosec * 3;
+    private static final long TIME_BETWEEN_OBJECTS = Framework.SEC_IN_NANOSEC;
+    private static long lastObjectTime;
+    private static int objectLine = 0;
+    private static final int[][] objectLines = {
+            {Framework.getFrameWidth() , (int)(Framework.getFrameHeight() * 0.20), -17, 50},
+            {Framework.getFrameWidth() , (int)(Framework.getFrameHeight() * 0.25), -16, 50},
+            {Framework.getFrameWidth() , (int)(Framework.getFrameHeight() * 0.30), -15, 50},
+            {Framework.getFrameWidth() , (int)(Framework.getFrameHeight() * 0.35), -14, 50}
+    };
 
-    public static int nextObjectLines = 0;
-    /**
-     * kr.jbnu.se.std.Duck lines.
-     * Where is starting location for the duck?
-     * Speed of the duck?
-     * How many points is a duck worth?
-     */
-    public static int[][] objectLines = {
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.20), -17, 50},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.25), -16, 50},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.30), -15, 50},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.35), -14, 50}
-                                      };
-    /**
-     * Creates new duck.
-     * @param objectImg Image of the duck.
-     */
-    public Hawk(BufferedImage objectImg)
-    {
-        super(objectLines, nextObjectLines, objectImg);
+    public Hawk() throws IOException { //예외처리ㅎ
+        super(GameModel.stage(GameConfig.getStage()).getDuckImg());
+        this.x = objectLines[objectLine][0] + this.random.nextInt(200);
+        this.y = objectLines[objectLine][1];
+        this.speed = objectLines[objectLine][2];
+        this.score = objectLines[objectLine][3];
+        updateObjectLine();
+    }
+
+    public static long getLastObjectTime() {
+        return lastObjectTime;
+    }
+
+    public static void setLastObjectTime(long time) {
+        lastObjectTime = time;
+    }
+
+    private static void updateObjectLine() {
+        if (objectLine < 3) {
+            objectLine++;
+        } else {
+            objectLine = 0;
+        }
+    }
+
+    public static long getTimeBetweenObjects() {
+        return TIME_BETWEEN_OBJECTS;
     }
 }
