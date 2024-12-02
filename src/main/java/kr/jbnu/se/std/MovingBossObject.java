cee770c1b4ff37228ae4b717eb5229d3c3a6b258
hyowon <khw0113@jbnu.ac.kr>
2024-12-02 14:49:26 +0900

    MovingBossObject

--- src/main/java/kr/jbnu/se/std/MovingBossObject.java
+++ src/main/java/kr/jbnu/se/std/MovingBossObject.java
@@ -5,32 +5,29 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.util.Random;
-import static kr.jbnu.se.std.Game.gunDamage;
 
 public class MovingBossObject {
-    protected static int FLIP_POSITION1;
-    protected static int FLIP_POSITION2;
     protected boolean flipped;
     protected int health; // 체력 추가
-    protected int INITIAL_HEALTH;
-    public int score;
-    public int x;
-    public int y;
-    public static Random random;
+    protected final int initialHealth;
+    private static int score;
+    private int x;
+    private final int y;
     protected BufferedImage objectImg;
     protected  BufferedImage correctImg;
     protected BufferedImage flippedImg;
     protected int speed;
     private static final int HEALTH_BAR_WIDTH = 100;
     private static final int HEALTH_BAR_HEIGHT = 10;
-    public int width;
-    public int height;
-    public String soundName;
-    public static int nextObjectLines = 0;
+    private final int width;
+    private final int height;
+    private int objectLine = 0;
+    protected static int flipPosition1;
+    protected static int flipPosition2;
 
-    public MovingBossObject(int[][] objectLines,int nextObjectLines,BufferedImage objectImg)
+    public MovingBossObject(int[][] objectLines, int health, BufferedImage objectImg)
     {
-        random = new Random();
+        Random random = new Random();
         flipped = false;
         correctImg = objectImg;
         AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
@@ -38,36 +35,66 @@ public class MovingBossObject {
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
         flippedImg = op.filter(objectImg, null);
         this.objectImg = correctImg;
-        this.x = objectLines[nextObjectLines][0] + random.nextInt(200);
-        this.y = objectLines[nextObjectLines][1];
-        this.speed = objectLines[nextObjectLines][2];
-        this.score = objectLines[nextObjectLines][3];
-        INITIAL_HEALTH = 100;
+        this.x = objectLines[objectLine][0] + random.nextInt(200);
+        this.y = objectLines[objectLine][1];
+        this.speed = objectLines[objectLine][2];
+        score = objectLines[objectLine][3];
+        initialHealth = health;
+        this.health = health;
         width = objectImg.getWidth();
         height = objectImg.getHeight();
-        FLIP_POSITION1 = 0;
-        FLIP_POSITION2 = 1200 - width;
+        flipPosition1 = 0;
+        flipPosition2 = 1200 - width;
+        updateObjectLine();
     }
 
-    public void Update() {
+    public void update() {
         if (!flipped) {
             x += speed;
-            if (x <= FLIP_POSITION1) {
+            if (x <= flipPosition1) {
                 flipped = true;
                 objectImg = flippedImg;
             }
         } else {
             x -= speed;
-            if (x >= FLIP_POSITION2) {
+            if (x >= flipPosition2) {
                 flipped = false;
                 objectImg = correctImg;
             }
         }
     }
 
-    public boolean hit(Game.GunTypes gunType) {
-        health-= gunDamage.get(gunType);
+    private void updateObjectLine() {
+        if (objectLine < 3) {
+            objectLine++;
+        } else {
+            objectLine = 0;
+        }
+    }
+
+    public boolean hit(GameConfig.GunType gunType) {
+        health-= Guns.getDamage(gunType);
         return health <= 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public void drawHealthBar(Graphics2D g2d) {
@@ -76,21 +103,21 @@ public class MovingBossObject {
         g2d.setColor(Color.GRAY);
         g2d.fillRect(x + objectImg.getWidth() / 2 - HEALTH_BAR_WIDTH / 2, healthBarY, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
 
-        if (health > 0.7 * INITIAL_HEALTH) {
+        if (health > 0.7 * initialHealth) {
             g2d.setColor(Color.GREEN);
-        } else if (health > 0.3 * INITIAL_HEALTH) {
+        } else if (health > 0.3 * initialHealth) {
             g2d.setColor(Color.YELLOW);
         } else {
             g2d.setColor(Color.RED);
         }
 
-        g2d.fillRect(x + objectImg.getWidth() / 2 - HEALTH_BAR_WIDTH / 2, healthBarY, (int) (HEALTH_BAR_WIDTH * (double)health/(double)INITIAL_HEALTH), HEALTH_BAR_HEIGHT);
+        g2d.fillRect(x + objectImg.getWidth() / 2 - HEALTH_BAR_WIDTH / 2, healthBarY, (int) (HEALTH_BAR_WIDTH * (double)health/(double)initialHealth), HEALTH_BAR_HEIGHT);
 
         g2d.setColor(Color.BLACK);
         g2d.drawRect(x + objectImg.getWidth() / 2 - HEALTH_BAR_WIDTH / 2, healthBarY, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
     }
 
-    public void Draw(Graphics2D g2d) {
+    public void draw(Graphics2D g2d) {
         g2d.drawImage(objectImg, x, y, null);
         drawHealthBar(g2d);
     }
