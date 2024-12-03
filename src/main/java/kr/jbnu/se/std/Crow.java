9e2d051172b8ecc33a212dd2c483d361c60f8672
hyowon <khw0113@jbnu.ac.kr>
2024-12-03 12:15:33 +0900

    refactor Crow

--- src/main/java/kr/jbnu/se/std/Crow.java
+++ src/main/java/kr/jbnu/se/std/Crow.java
@@ -1,18 +1,16 @@
 package kr.jbnu.se.std;
 
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Crow extends MovingBossObject {
-    public static int[][] objectLines = {
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.30), -5, 500},
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.35), -6, 500},
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.40), -7, 500},
-            {Framework.frameWidth, (int)(Framework.frameHeight * 0.45), -8, 500}
+    private static final int[][] objectLines = {
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.30), -5, 500},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.35), -6, 500},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.40), -7, 500},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.45), -8, 500}
     };
 
-    public Crow(BufferedImage objectImg) {
-        super(objectLines,nextObjectLines,objectImg);
-        INITIAL_HEALTH = 30;
-        this.health = INITIAL_HEALTH;
+    public Crow() throws IOException {
+        super(objectLines ,30, GameModel.stage(GameConfig.getStage()).getBossImg());
     }
 }
