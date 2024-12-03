8a8bca228c1bc59442f67969f1da8e3929512729
Kimyunsu <yunsu3375@naver.com>
2024-12-03 13:19:04 +0900

    Hawk

--- src/main/java/kr/jbnu/se/std/Witch.java
+++ src/main/java/kr/jbnu/se/std/Witch.java
@@ -1,39 +1,42 @@
 package kr.jbnu.se.std;
 
-import java.awt.image.BufferedImage;
-
-/**
- * The duck class.
- * 
- * @author www.gametutorial.net
- */
-
 public class Witch extends MovingObject {
-    public static long lastObjectTime;
-    /**
-     * How much time must pass in order to create a new eagle?
-     */
-    public static long timeBetweenObjects = Framework.secInNanosec * 3;
+    private static final long TIME_BETWEEN_OBJECTS = Framework.SEC_IN_NANOSEC;
+    private static long lastObjectTime;
+    private static int objectLine = 0;
+    private static final int[][] objectLines = {
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.10), -20, 200},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.3), -20, 200},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.4), -20, 200},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.5), -20, 200}
+    };
 
-    public static int nextObjectLines = 0;
-    /**
-     * kr.jbnu.se.std.Duck lines.
-     * Where is starting location for the duck?
-     * Speed of the duck?
-     * How many points is a duck worth?
-     */
-    public static int[][] objectLines = {
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.10), -20, 200},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.3), -20, 200},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.4), -20, 200},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.5), -20, 200}
-                                      };
-    /**
-     * Creates new duck.
-     * @param objectImg Image of the duck.
-     */
-    public Witch(BufferedImage objectImg)
-    {
-        super(objectLines, nextObjectLines, objectImg);
+    public Witch() {
+        super(GameModel.getWitchImg());
+        this.x = objectLines[objectLine][0] + this.random.nextInt(200);
+        this.y = objectLines[objectLine][1];
+        this.speed = objectLines[objectLine][2];
+        this.score = objectLines[objectLine][3];
+        updateObjectLine();
+    }
+
+    public static long getLastObjectTime() {
+        return lastObjectTime;
+    }
+
+    public static void setLastObjectTime(long time) {
+        lastObjectTime = time;
+    }
+
+    private static void updateObjectLine() {
+        if (objectLine < 3) {
+            objectLine++;
+        } else {
+            objectLine = 0;
+        }
+    }
+
+    public static long getTimeBetweenObjects() {
+        return TIME_BETWEEN_OBJECTS;
     }
 }
