bdfcd2bd2bfacc30228f509486a2032e04435f3e
Kimyunsu <yunsu3375@naver.com>
2024-12-03 13:16:06 +0900

    Duck

--- src/main/java/kr/jbnu/se/std/Duck.java
+++ src/main/java/kr/jbnu/se/std/Duck.java
@@ -1,22 +1,54 @@
 package kr.jbnu.se.std;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Duck extends MovingObject {
-    public static long lastObjectTime;
-    public static long timeBetweenObjects = Framework.secInNanosec;
+    private static final long TIME_BETWEEN_OBJECTS = Framework.SEC_IN_NANOSEC;
+    private static long lastObjectTime;
+    private static int objectLine = 0;
+    private static final int[][] objectLines = {
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.60), -8, 20},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.65), -9, 30},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.70), -10, 40},
+            {Framework.getFrameWidth(), (int)(Framework.getFrameHeight() * 0.78), -11, 50}
+    };
 
-    public static int nextObjectLines = 0;
+    public Duck() throws IOException {
+        super(GameModel.stage(GameConfig.getStage()).getDuckImg());
+        this.x = objectLines[objectLine][0] + this.random.nextInt(200);
+        this.y = objectLines[objectLine][1];
+        this.speed = objectLines[objectLine][2];
+        this.score = objectLines[objectLine][3];
+        updateObjectLine();
+    }
 
-    public static int[][] objectLines = {
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.60), -2, 20},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.65), -3, 30},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.70), -4, 40},
-                                       {Framework.frameWidth, (int)(Framework.frameHeight * 0.78), -5, 50}
-                                      };
+    protected Duck(BufferedImage image){
+        super(image);
+        this.x = objectLines[objectLine][0] + this.random.nextInt(200);
+        this.y = objectLines[objectLine][1];
+        this.speed = -15;
+        this.score = 50;
+        updateObjectLine();
+    }
 
-    public Duck(BufferedImage objectImg)
-    {
-        super(objectLines, nextObjectLines, objectImg);
+    public static long getLastObjectTime() {
+        return lastObjectTime;
+    }
+
+    public static void setLastObjectTime(long time) {
+        lastObjectTime = time;
+    }
+
+    private static void updateObjectLine() {
+        if (objectLine < 3) {
+            objectLine++;
+        } else {
+            objectLine = 0;
+        }
+    }
+
+    public static long getTimeBetweenObjects() {
+        return TIME_BETWEEN_OBJECTS;
     }
 }
